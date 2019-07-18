32931c554bcc349a311996bfa90c9c31859abb09
Royi Yaakoby <royiyaakoby@Royis-iMac.local>
2019-07-18 03:02:52 +0300

    replacing with ~Hash~ Level 4

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+var md5 = require('md5');
+
 
 const app = express();
 
@@ -17,7 +19,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -42,7 +44,7 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
 const userName = req.body.username;
-const password = req.body.password;
+const password = md5(req.body.password);
 
 User.findOne({email:userName}, function(err, foundUser){
   if (err){
@@ -69,7 +71,7 @@ app.post("/register", function(req, res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (err) {
